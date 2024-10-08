d440bb0c21ae2c2769588b680763ab08456a7614
deveon95 <51299713+deveon95@users.noreply.github.com>
2024-10-08 18:05:04 +0100

    Split config into separate file so that main code can be easily updated without having to change the config every time

--- slideomatic.py
+++ slideomatic.py
@@ -19,60 +19,11 @@
 # any newly added files to be included.
 
 ## Configuration variables
-# Number of different local music playlists
-localmusicnumber = 2
-# Specified attribute to use for searching for each local music programme
-localmusicsearchfield = ["Artist", "Album"]
-# Value of specified attribute used to identify local music for inclusion
-localmusicsearchdata = ["Ceefax", "Mix"]
-# Path and file match criteria for slideshow images
-slideshowpath = "/home/pi/slideshow/*.jpg"
-# Commands for formatting the status output for the caption
-localmusicstatuscommand = ['mpc', '-f', '%album%: %title%', 'current']
-internetradiostatuscommand = ['mpc', 'current']
-# Offsets for correcting image position so that it appears in the centre
-vbioffset = 24
-horoffset = 9
-# Background colours
-defaultbg = (0,0,0)
-defaultbgcaption = defaultbg
-textfg = (191,191,191)
-# display current programme number on screen
-enableprogrammenumber = True
-# Margin around caption (left and right) for overscan
-captionmargin = 40
-# Font height in pixels
-fontsize = 40
-# Caption height in pixels
-captionheight = (fontsize * 2)
-# GPIO pins for the programme selection switch (N.B. Pin 27 on later Pis is pin 21 on original Pi)
-switchAGPIO = 12
-switchBGPIO = 16
-switchCGPIO = 20
-switchDGPIO = 21
-# Audio enable GPIO for switching the audio mux between master audio and this Pi
-audioEnGPIO = 26
-useAudioEn = False
-# Fixed deduction due to font.size returning value with blank line of pixels added
-fontHeightDeduct = 1
-# Path to font file
-fontpath = "/usr/share/fonts/teletext4.ttf"
-# Timebase in seconds for all operations including sampling the programme switch
-timebase = 0.1
-# Interval for running mpc command to get caption (in units of timebase)
-captioninterval = (2 * 10)
-# Interval for changing the slideshow image (in units of timebase)
-imageinterval = ((60 * 10) * 10)
-# Set switchinvert to True if switch contacts are closed when set to 0 (disconnected switch will also be read as 15),
-# or False if switch contacts are open when set to 0 (disconnected switch will also be read as 0)
-switchinvert = False
-# number of consistent samples of the switch required to change the programme;
-# this prevents unnecessary server requests being made when moving the switch multple positions
-# and protects against the spurious outputs that BCD-coded thumbwheel switches generate when incrementing
-switchsamplecount = 10
+# Moved to slideomaticconfig.py
 
 import os,sys,pygame,subprocess,glob,random,time,signal,gpiozero,array
 from unicodedata import normalize
+from slideomaticconfig import *
 
 # Sigterm handler
 # To resolve problem of hanging at display.set_mode until Ctrl+C is pressed on subsequent runs
